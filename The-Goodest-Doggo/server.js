a8345494d787c3a5036f9a4861ff08194b7ec403
Robert Perillo <perillo.robert@gmail.com>
2020-02-29 15:56:20 -0700

    create OAuth functionality

--- The-Goodest-Doggo/server.js
+++ The-Goodest-Doggo/server.js
@@ -2,6 +2,8 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var passport = require('passport');
 var logger = require('morgan');
 
 require('dotenv').config();
@@ -12,6 +14,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 require('./config/database');
+require('./config/passport');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,11 +23,21 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+
+app.use(session({
+  secret: 'SEIProject2',
+  resave: false,
+  saveUninitialized: true
+ }));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
