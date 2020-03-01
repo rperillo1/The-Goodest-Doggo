30c4087a2dd69b92445dbb31205008ccbc38a999
Robert Perillo <perillo.robert@gmail.com>
2020-02-29 17:54:34 -0700

    fix google oauth module

--- The-Goodest-Doggo/routes/index.js
+++ The-Goodest-Doggo/routes/index.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var passport = require('passport');
-
+const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.redirect('/home');
 });
-
 
 //Google OAuth login route
 router.get('/auth/google', passport.authenticate(
@@ -19,15 +17,15 @@ router.get('/auth/google', passport.authenticate(
 router.get('/oauth2callback', passport.authenticate(
   'google',
   {
-    successRedirect : '/',
-    failureRedirect : '/'
+    successRedirect : '/home',
+    failureRedirect : '/home'
   }
 ));
 
 //OAuth logout route
 router.get('/logout', function(req, res){
   req.logout();
-  res.redirect('/students');
+  res.redirect('/home');
 });
 
 
