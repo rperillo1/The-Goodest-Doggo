1198398c4adea9a075ffb493100a6fbecade1c18
Robert Perillo <perillo.robert@gmail.com>
2020-03-03 09:29:56 -0700

    add bracket id to index file

--- controllers/competing-dogs.js
+++ controllers/competing-dogs.js
@@ -2,20 +2,40 @@ const User = require('../models/user');
 const CompetingDog = require('../models/competing-dog')
 const Bracket = require('../models/bracket');
 
+// function index(req, res, next) {
+//     console.log(req.params.userId);
+//     Bracket.find({})
+//     User.find({}, function(err, user){
+//         bracket.save(function(err, bracket){
+//             const competingDog = new CompetingDog({
+//                 bracket: bracket._id,
+//                 user: req.params.userId
+//             });
+//             console.log(competingDog);
+//             res.render('bracket/index', {
+//                user
+//             }); 
+//         })
+//     });
+// }
+
 function index(req, res, next) {
     console.log(req.params.userId);
     const bracket = new Bracket();
-    bracket.save(function(err, bracket){
-        const competingDog = new CompetingDog({
-            bracket: bracket._id,
-            user: req.params.userId
-        });
-        console.log(competingDog);
-        res.render('bracket/index', {
-           
-        }); 
-    })
+    User.find({}, function(err, user){
+        bracket.save(function(err, bracket){
+            const competingDog = new CompetingDog({
+                bracket: bracket._id,
+                user: req.params.userId
+            });
+            console.log(competingDog);
+            res.render('bracket/index', {
+               user
+            }); 
+        })
+    });
 }
+
 
 
 
